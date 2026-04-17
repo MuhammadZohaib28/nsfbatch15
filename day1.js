735425baa77748c539f0f217fcaa44f4841750f2
Muhammad Zohaib <zohaibsoomro100@gmail.com>
2026-04-17 22:07:46 +0500

    JavaScript day 1

---
+++ day1.js
@@ -0,0 +1,46 @@
+console.log("10 + 10")
+
+
+// DATA TYPES OF JAVASCRIPT
+// STRING
+// NUMBER
+// BOOLEAN
+// BIGINT
+// ARRAY
+// OBJECT
+
+
+console.log("20 + 20 hota hai 40")
+
+
+
+console.log(20 + 30, "Mene yahan 20 + 30 kiya hai")
+console.log(20 - 30, "yahan mene 20 - 30 kiya hai")
+
+
+console.log(10 + 30 + "10" + 20 + 20, "CONCATENATION")
+
+console.log(30 + 10)
+
+
+console.log("50" + "30", "YAHAN MINUS HO RAHA HAI STRINGS")
+
+
+
+const userAge = 26;
+
+const userName = "Zulqarnain"
+
+
+console.log("Name of the user is " +  userName + " and the user age is " + userAge)
+
+
+
+
+console.log("Zulqarnain" + 20)
+
+
+
+
+console.log(true)
+console.log(false)

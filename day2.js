48d95a7eb752c83080a7f5c9475e708ec44f2e16
Muhammad Zohaib <zohaibsoomro100@gmail.com>
2026-04-20 21:56:17 +0500

    js day 2

---
+++ day2.js
@@ -0,0 +1,59 @@
+console.log("Hello, World! This is Day 2 of learning JavaScript.");
+
+// VARIABLE IN JAVASCRIPT
+// VAR
+// LET
+// CONST
+
+// naming convention
+
+// myNameIs - CAMEL CASE CONVENTION
+// my-name-is - KEBAB CASE CONVENTION
+// my_name_is - SNAKE CASE CONVENTION
+
+
+
+var userEmail = "munzir@gmail.com";
+var userPassword = "newpassword"
+
+
+// DRY - DO NOT REPEAT YOURSELF
+
+console.log("zohaibsoomro100@gmail.com")
+
+console.log(userEmail, "YEH USER KI EMAIL HAI")
+
+console.log("Password:", userPassword)
+
+
+var updatedPassword = userPassword;
+
+
+console.log("Updated Password:", updatedPassword)
+
+updatedPassword = "yehm  %&& eranew    passwordh  ai"
+
+console.log("NEW PASSWORD:", updatedPassword)
+
+
+
+
+let userAge = 22;
+
+console.log(userAge)
+
+let hours = new Date().getHours();
+let twelveClockHours = hours - 12
+let mins = new Date().getMinutes();
+
+let sec = new Date().toUTCString();
+
+console.log(sec)
+
+
+console.log(twelveClockHours + ":" + mins)
+
+
+
+
+let meraNaam = "Munzir"

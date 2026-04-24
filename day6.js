ea6e6997fc1b09788bd049bf9086224d91178eba
Muhammad Zohaib <zohaibsoomro100@gmail.com>
2026-04-24 22:09:37 +0500

    for loops

---
+++ day6.js
@@ -0,0 +1,47 @@
+// LOOPS 
+// for loop
+// while loop
+// do while
+
+
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// 11
+// 12
+// 13
+// 14
+// 15
+
+
+
+// DRY - DO NOT REPEAT YOURSELF
+
+
+// for (let i = 1; i <= 1000; i++) {
+//     console.log(i)
+// }
+
+
+
+
+
+// 4 x 1 = 4
+// 4 x 2 = 8
+// 4 x 3 = 12
+
+
+
+let tableNumber = prompt("Please enter any table number");
+
+
+for (let i = 1; i <= 20; i++){
+    console.log(`${tableNumber} x ${i} = ${tableNumber*i}`)
+}
